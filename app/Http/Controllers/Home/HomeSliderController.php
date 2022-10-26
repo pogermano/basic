d63f974a5d3bde4f10ac6cfa39ca1ec6f8964cc1
pogermano <pogermano@gmail.com>
2022-10-26 14:44:35 -0300

    Backend Home Slide

--- app/Http/Controllers/Home/HomeSliderController.php
+++ app/Http/Controllers/Home/HomeSliderController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Home;
 use App\Models\HomeSlide;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Image;
+
+
+
 
 class HomeSliderController extends Controller
 {
@@ -11,5 +15,48 @@ class HomeSliderController extends Controller
         $homeslide = HomeSlide::find(1);
         return view('admin.home_slide.home_slide_all', compact('homeslide'));
 
-   }// End Method
+   } // End Method
+
+    public function UpdateSlider(Request $request)
+    {
+
+        $slide_id = $request->id;
+
+        if ($request->file('home_slide')) {
+            $image = $request->file('home_slide');
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();  // 3434343443.jpg
+
+            Image::make($image)->resize(636, 852)->save('upload/home_slide/' . $name_gen);
+            $save_url = 'upload/home_slide/' . $name_gen;
+
+            HomeSlide::findOrFail($slide_id)->update([
+                'title' => $request->title,
+                'short_title' => $request->short_title,
+                'video_url' => $request->video_url,
+                'home_slide' => $save_url,
+
+            ]);
+            $notification = array(
+                'message' => 'Home Slide Updated with Image Successfully',
+                'alert-type' => 'success'
+            );
+
+            return redirect()->back()->with($notification);
+        } else {
+
+            HomeSlide::findOrFail($slide_id)->update([
+                'title' => $request->title,
+                'short_title' => $request->short_title,
+                'video_url' => $request->video_url,
+
+            ]);
+            $notification = array(
+                'message' => 'Home Slide Updated without Image Successfully',
+                'alert-type' => 'success'
+            );
+
+            return redirect()->back()->with($notification);
+        } // end Else
+
+    } // End Method
 }
--- resources/views/admin/home_slide/home_slide_all.blade.php
+++ resources/views/admin/home_slide/home_slide_all.blade.php
@@ -1,31 +1,40 @@
 @extends('admin.admin_master')
 @section('admin')
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <div class="page-content">
-        <div class="container-fluid">
+
+<div class="page-content">
+<div class="container-fluid">
+
+<div class="row">
+<div class="col-12">
+    <div class="card">
+        <div class="card-body">
+
+            <h4 class="card-title">Home Slide Page </h4>
+
+            <form method="post" action="{{ route('update.slider') }}" enctype="multipart/form-data">
+                @csrf
+
+                <input type="hidden" name="id" value="{{ $homeslide->id }}">
+
+            <div class="row mb-3">
+                <label for="example-text-input" class="col-sm-2 col-form-label">Title</label>
+                <div class="col-sm-10">
+                    <input name="title" class="form-control" type="text" value="{{ $homeslide->title }}"  id="example-text-input">
+                </div>
+            </div>
+            <!-- end row -->
+
+              <div class="row mb-3">
+                <label for="example-text-input" class="col-sm-2 col-form-label">Short Title </label>
+                <div class="col-sm-10">
+                    <input name="short_title" class="form-control" type="text" value="{{ $homeslide->short_title }}"  id="example-text-input">
+                </div>
+            </div>
+            <!-- end row -->
 
 
-            <div class="card-body">
-                <h4 class="card-title">Home Slide Page</h4>
-                <form method="POST" action="{{ route('store.profile') }}" enctype="multipart/form-data">
-                    @csrf
-                    <div class="row mb-3 mt-4">
-                        <label for="title" class="col-sm-2 col-form-label">Title</label>
-                        <div class="col-sm-10">
-                            <input class="form-control" type="text" id="title" value="{{ $homeslide->title }}"
-                                name="title">
-                        </div>
-                    </div>
-                    <!-- end row -->
-                    <div class="row mb-3">
-                        <label for="short_title" class="col-sm-2 col-form-label">Short Title</label>
-                        <div class="col-sm-10">
-                            <input class="form-control" type="short_title" id="text" value="{{ $homeslide->short_title }}"
-                                name="short_title">
-                        </div>
-                    </div>
-                    <!-- end row -->
-                      <div class="row mb-3">
+              <div class="row mb-3">
                 <label for="example-text-input" class="col-sm-2 col-form-label">Video URL </label>
                 <div class="col-sm-10">
                     <input name="video_url" class="form-control" type="text" value="{{ $homeslide->video_url }}"  id="example-text-input">
@@ -45,7 +54,7 @@
               <div class="row mb-3">
                  <label for="example-text-input" class="col-sm-2 col-form-label">  </label>
                 <div class="col-sm-10">
-                    <img id="showImage" class="rounded avatar-lg" src="{{ (!empty($homeslide->home_slide))? url('upload/home_slide/'.$homeslide->home_slide):url('upload/no_image.jpg') }}" alt="Card image cap">
+  <img id="showImage" class="rounded avatar-lg" src="{{ (!empty($homeslide->home_slide))? url( $homeslide->home_slide):url('upload/no_image.jpg') }}" alt="Card image cap">
                 </div>
             </div>
             <!-- end row -->
@@ -76,6 +85,7 @@
             reader.readAsDataURL(e.target.files['0']);
         });
     });
+
 </script>
 
 @endsection
