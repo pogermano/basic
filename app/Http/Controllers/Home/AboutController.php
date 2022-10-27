128112ed512bbe77169e4b7df0bc59bc3c755ee8
pogermano <pogermano@gmail.com>
2022-10-27 12:09:03 -0300

    About page start

---
+++ app/Http/Controllers/Home/AboutController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use App\Http\Controllers\Controller;
+use App\Models\About;
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function AboutPage()
+    {
+
+        $aboutpage = About::find(1);
+        return view('admin.about_page.about_page_all', compact('aboutpage'));
+    } // End Method
+}
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\AdminController;
 
 use App\Http\Controllers\Home\HomeSliderController;
 
+use App\Http\Controllers\Home\AboutController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -41,6 +43,13 @@ Route::controller(AdminController::class)->group(function(){
 Route::controller(HomeSliderController::class)->group(function(){
         Route::get('/home/slide',  'HomeSlider')->name('home.slide');
         Route::post('/update/slider', 'UpdateSlider')->name('update.slider');
+});
+
+
+//About Page All Route
+Route::controller(AboutController::class)->group(function(){
+        Route::get('/about/page',  'AboutPage')->name('about.page');
+
 });
 
 
