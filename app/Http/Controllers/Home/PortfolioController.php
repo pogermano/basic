bb5985fc9fac14213d54e2819c21438748aac10e
Pedro Germano <pogermano@gmail.com>
2022-11-02 12:58:55 -0300

    Portfolio page

--- app/Http/Controllers/Home/PortfolioController.php
+++ app/Http/Controllers/Home/PortfolioController.php
@@ -128,4 +128,10 @@ class PortfolioController extends Controller
 
         return redirect()->back()->with($notification);
     }// End Method 
+
+    public function PortfolioDetails($id){
+
+        $portfolio = Portfolio::findOrFail($id);
+        return view('frontend.protfolio_details',compact('portfolio'));
+     }// End Method 
 }
--- resources/views/frontend/home_all/portfolio.blade.php
+++ resources/views/frontend/home_all/portfolio.blade.php
@@ -29,9 +29,9 @@ $portfolio = App\Models\Portfolio::latest()->get();
         <img src="{{ asset($item->portfolio_image) }}" alt="">
     </div>
     <div class="portfolio__overlay__content">
-        <span>{{$item->portfolio_name}}</span>
-        <h4 class="title"><a href="">{{$item->  portfolio_title}}</a></h4>
-        <a href="" class="link">Case Study</a>
+        <span>{{$item->portfolio_name}}</span>       
+        <h4 class="title"><a href="{{ route('portfolio.details',$item->id)}}">{{$item->  portfolio_title}}</a></h4>
+        <a href="{{ route('portfolio.details',$item->id)}}" class="link">Case Study</a>
     </div>
     </div> 
     @endforeach
